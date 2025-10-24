cdcefe2437af71770c423f31188b3d0895aa4fca
Ramprasadvaral13 <136104228+Ramprasadvaral13@users.noreply.github.com>
2025-10-24 18:40:47 +0530

    Update variable.tf

--- compute/variable.tf
+++ compute/variable.tf
@@ -34,3 +34,9 @@ variable "volume_size" {
     type = number
   
 }
+
+variable "key_pair_name" {
+  description = "Name of the EC2 key pair"
+  type        = string
+  default     = "cloudtechdev1.pem"
+}
